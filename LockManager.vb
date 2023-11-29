1295711d0168f35ecc61f975f115719760845bed
Charles Paldino <charlespaldino@protonmail.com>
2023-11-29 00:53:00 -0500

    Initial upload

---
+++ LockManager.vb
@@ -0,0 +1,106 @@
+﻿Imports Microsoft.VisualBasic
+
+''' <summary>
+''' This is sample code, assumes various objects exist.
+''' </summary>
+Public Class LockManager
+
+    Private Shared Property EditLockList As Dictionary(Of String, Lock) = New Dictionary(Of String, Lock)
+
+    ''' <summary>
+    ''' Adds this edit lock to the list.
+    ''' </summary>
+    Public Shared Sub addEditLock(pagelock As Lock)
+        If Not EditLockList.ContainsKey(pagelock.Page) Then
+            EditLockList.Add(pagelock.Page, pagelock)
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Adds this a new edit lock to the list.
+    ''' </summary>
+    Public Shared Sub addEditLock(page As String, sessionid As String, username As String)
+        If Not EditLockList.ContainsKey(page) Then
+            EditLockList.Add(page, New Lock(page, sessionid, username))
+        Else
+            EditLockList(page).refreshExpirationTimer()
+        End If
+    End Sub
+
+
+    ''' <summary>
+    ''' Refreshes  this edit lock.
+    ''' </summary>
+    Public Shared Sub refreshEditLock(page As String)
+        If EditLockList.ContainsKey(page) Then
+            EditLockList(page).refreshExpirationTimer()
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Removes this edit lock from the list.
+    ''' </summary>
+    Public Shared Sub removeEditLock(pagelock As Lock)
+        EditLockList.Remove(pagelock.Page)
+    End Sub
+
+    ''' <summary>
+    ''' Removes this edit lock from the list based off of the given page.
+    ''' </summary>
+    Public Shared Sub removeEditLock(page As String)
+        EditLockList.Remove(page)
+    End Sub
+
+    ''' <summary>
+    ''' Removes all edit locks linked to this username.
+    ''' </summary>
+    Public Shared Sub removeEditLocksByUser(username As String)
+        For Each editlock In EditLockList.Values.ToList()
+            If editlock.UserName Is Nothing Then
+                EditLockList.Remove(editlock.Page)
+                Return
+            End If
+
+            If editlock.UserName.Equals(username) Then
+                EditLockList.Remove(editlock.Page)
+            End If
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' Checks if this page has been locked.
+    ''' </summary>
+    ''' <returns>True, if this page is in the locked list.</returns>
+    Public Shared Function isEditLocked(page As String) As Boolean
+        Return If(EditLockList.ContainsKey(page), Not EditLockList(page).isTimerExpired(), False)
+    End Function
+
+    ''' <summary>
+    ''' Checks if this page is locked by the given user.
+    ''' </summary>
+    ''' <param name="page">The page to check.</param>
+    ''' <param name="username">The username to check</param>
+    ''' <returns>True, if this page is locked by the given user and not expired.</returns>
+    Public Shared Function isEditLockedByUser(page As String, username As String) As Boolean
+        If EditLockList.ContainsKey(page) Then
+            If EditLockList(page).UserName.Equals(username) Then
+                Return True
+            Else
+                Return EditLockList(page).isTimerExpired()
+            End If
+        End If
+
+        Return False
+    End Function
+
+    ''' <summary>
+    ''' Gets the username of the user holding the lock for this page.
+    ''' </summary>
+    ''' <returns>N/A, if not found.</returns>
+    Public Shared Function getEditLockUsername(page As String) As String
+        Dim editlock As Lock = EditLockList(page)
+
+        Return If(editlock Is Nothing, "N/A", editlock.UserName)
+    End Function
+
+End Class
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# DotNet-VB
+# .Net-VB
 Code repo for .Net VB related code samples
+
+Skills Demonstrated:
+.Net VB
+FTP
+FILE IO
+Salt, Hash, and Encryption.
+
+
+Classes:
+=FTPManager
+Handles sending an order and its products to an FTP server, for the end client to process.
+
+=LockManager
+Handles locking down pages on systems with concurrent users. 
+This is a tool that can tell users the page is currently in use and can not be edited or viewed.
+This assumes there is a Lock object that stores the page, sessionid, and username.
+
+=Security Utils
+I pulled a guide from online and tooled up salting, hashing, and encryption for use on sensitive data.
+For ease of use, there is one main entry method, with exposed helped methods.
+This also includes a basic tool to help clean any SQL Injection.
